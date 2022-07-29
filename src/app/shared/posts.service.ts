79d74009a6882c2618ac9a87563413b42908a3c5
DJUR-DJUR <djurodjur@gmail.com>
2022-07-29 22:05:46 +0300

    dashbord add all post DONE

--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map, Observable } from "rxjs";
+import { map, Observable, pipe } from "rxjs";
 import { keys } from "../keys/keys";
 import { FbCreateResponse, Post } from "./interfases";
 
@@ -16,6 +16,19 @@ export class PostsService {
                     id: response.name,
                     date: new Date(post.date)
                 }
+            }))
+    }
+
+    getAll(): Observable<Post[]> {
+        return this.http.get(`${keys.fbDbUrl}/posts.json`)
+            .pipe(map( (response: {[key:string]: any}) => {
+                return Object
+                .keys(response)
+                .map( key => ({
+                    ...response[key],
+                    id: key,
+                    date: new Date(response[key].date)
+                }))
             }))
     }
 }
