3682c98d1df645439827dc51e98a867ec5817f38
DJUR-DJUR <djurodjur@gmail.com>
2022-07-30 17:08:33 +0300

    local date DONE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule, Provider } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import  ruLocale  from '@angular/common/locales/ru-UA';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +13,9 @@ import { PostComponent } from './shared/components/post/post.component';
 import { SharedModule } from './shared/shares.module';
 import { AuthInterceptor } from './shared/auth.interceptor';
 
+
+
+registerLocaleData(ruLocale, 'ru')
 
 const INTERCEPTOR_PROVIDER: Provider = {
   provide: HTTP_INTERCEPTORS,
